a3ba6b3787d1b665c814e93eba9055dca57792f1
dariushine <frederic_788@hotmail.com>
2020-05-06 15:49:44 -0400

    added japanese side, and commented code

--- server/app.js
+++ server/app.js
@@ -12,13 +12,15 @@ const limiter = rateLimit({
   max: 100 // limit each IP to 100 requests per windowMs
 });
 
+// elona chat types
 const chat_types = {
+  // num to text
   'num': {
     0: 'chat',
     1: 'dead',
     2: 'wish'
   },
-
+  // text to num
   'name': {
     'chat': 0,
     'dead': 1,
@@ -29,23 +31,35 @@ const chat_types = {
 app.use([morgan('dev'), limiter]);
 app.disable('x-powered-by');
 
-app.get('/', (req, res) => res.send('Hello Worldes!'));
+app.get('/', (req, res) => res.send('Welcome to this Elona Chat server. Nothing in this page yet.'));
 
+// not sure what it does. is it like a motd? is it the voting board message?
 app.get('/text.txt', function (req, res) {
   response = "<!--START-->\n%\n素敵な異名コンテスト♪1  [１ヶ月で自動リセット]%\nYour favorite alias♪1  [Auto reset every month]%";
   res.set({ 'Content-type': 'text/plain' });
   res.send(response);
 });
 
-app.get('/logen.txt', function (req, res) {
-  pool.query("select * from chat where lang='en' order by id desc limit 30").then((result) => {
+app.get('/log(en)?.txt', function (req, res) {
+  // set language
+  let language = (req.path == '/log.txt' ? 'jp' : 'en')
+
+  // make query object
+  let query = {
+    name: 'fetch-log',
+    text: 'select * from chat where lang=$1 order by id desc limit 30',
+    values: [language]
+  }
+
+  // send query
+  pool.query(query).then((result) => {
     let response = "";
     let first = result.rowCount > 0 ? result.rows[0]['id'] : 1;
     response += first + "<C>\n<!--START-->\n";
     response += result.rows.reduce(function (text, row) {
       let date = row['time'];
 
-      // Move all of this to a function, dammit
+      // format date
       let dateObj = {
         day: date.getDate().toString(),
         month: date.getMonth().toString(),
@@ -53,14 +67,17 @@ app.get('/logen.txt', function (req, res) {
         minutes: date.getMinutes().toString()
       };
 
+      // padstart time variables
       for (val in dateObj) {
         dateObj[val] = dateObj[val].padStart(2, 0);
       };
 
+      // put everything in elona time format
       let dateText = `${dateObj.month}/${dateObj.day}(${dateObj.hour}:${dateObj.minutes})`;
-
+      
+      // format entire line
       text += row['id'] + '%' + dateText + '%' + chat_types.num[row['kind']] +
-        row['text'] + '%' + row['addr'] + '%\n';
+        row['text'] + '%' + '127.0.0.1' + '%\n';
       return text;
     }, "");
     response += "<!--END-->\n<!-- WebTalk v1.6 --><center><small><a href='http://www.kent-web.com/' target='_top'>WebTalk</a></small></center>";
@@ -70,33 +87,43 @@ app.get('/logen.txt', function (req, res) {
     .catch(e => console.log(e.stack));
 });
 
-app.get('/cgi-bin/wtalken/wtalk2.cgi', function (req, res) {
+app.get('/cgi-bin/wtalk(en)?/wtalk2.cgi', function (req, res) {
+  console.log(req.path)
+  let language = (req.path == '/cgi-bin/wtalk/wtalk2.cgi' ? 'jp' : 'en')
   let mode = req.query.mode;
   let comment = req.query.comment;
 
+  // check if the message is at least 1 character long (the first 4 are the type)
   if (comment == undefined || comment.length < 5) {
     res.status(400).send('400 Bad request');
     return;
   }
-
+  // check if the message has a valid type
   let chat_name = comment.substr(0, 4);
   if (!chat_types.name.hasOwnProperty(chat_name)) {
     res.status(400).send('400 Bad request');
     return;
   }
 
+  // format data
   let chat_type = chat_types.name[chat_name];
   let text = comment.substr(4);
   let addr = req.ip;
-  let curr_time = new Date(); //insert current time here
+  let curr_time = new Date();
+
   let query = {
-    name: 'fetch-log-en',
+    name: 'insert-log',
     text: 'INSERT INTO CHAT (time, kind, text, addr, lang) VALUES ($1, $2, $3, $4, $5)',
-    values: [curr_time, chat_type, text, addr, "en"]
+    values: [curr_time, chat_type, text, addr, language]
   };
 
+  // send query
   pool.query(query).catch(e => console.log(e.stack));
-  res.redirect('/logen');
+
+  // redirect depending on language
+  let logurl = (language == 'jp' ? '/log.txt' : '/logen.txt');
+  res.redirect(logurl);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// start app
+app.listen(port, () => console.log(`Elona Chat listening on port ${port}!`));
